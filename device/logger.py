2cd87b2df176108bd5931fe7479ebcea4704fe71
Chris Clearfield <chris.clearfield@gmail.com>
2018-06-24 10:28:59 -0700

    Now logging Flight model

--- device/logger.py
+++ device/logger.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 import urllib, json
-from device.models import AhrsSnapshot
+from device.models import Flight, Situation
 
 class StratuxLogger(): 
     def __init__(self, db, stratux_hostname="localhost", 
@@ -12,22 +13,42 @@ class StratuxLogger():
         self.current_snapshot = {}
         return
 
-    def getSituation(self): 
+    def getSituationFromStratux(self): 
         parameters = {}
         url = self.base_url + "getSituation"
-        print(url)
         response = urllib.request.urlopen(url)
         data = json.loads(response.read())
         self.current_snapshot = data;
         return data
 
-    def saveSnapshotToDb(self): 
+    def saveSnapshotToDb(self, flight_id): 
         if not self.current_snapshot:
-            self.getSituation()
+            self.getSituationFromStratux()
 
-        ahrs_snapshot = AhrsSnapshot()
-        ahrs_snapshot.GPSVerticalSpeed = \
-            self.current_snapshot['GPSVerticalSpeed']
-        self.db.session.add(ahrs_snapshot)
+
+        situation = Situation(**self.current_snapshot)
+        situation.StratuxTimeStamp = datetime.now()
+        situation.flight_id = flight_id
+        self.db.session.add(situation)
+
+    def logFlight(self): 
+        # Setup flight
+        # So we're going to just store everything in a 
+        # TZ ignorant object now; we can convert later to
+        # a TZ Aware object with pytz if we need to
+        flight_start = datetime.now()
+
+        flight = Flight()
+        flight.flight_start = flight_start
+        flight.n_number = "N12345" # XXX Make a config option
+        self.db.session.add(flight)
+        self.db.session.flush()
+        
+        return flight.id
+        
+
+        #self.getSituationFromStratux()
+        #self.saveSnapshotToDb()
+
 
     
